492590fddd6edfec71413fd0c22f38082a02a211
oheunjeong <55427561+oheunjeong@users.noreply.github.com>
2019-10-31 15:30:36 +0900

    09

--- main.c
+++ main.c
@@ -5,15 +5,24 @@
 
 int main(int argc, char *argv[]) {
 	
-	int data [5];
-	int i;
 	
-	data[0]=10;
-	data[1]=20;
-	data[2]=30;
-	data[3]=40;
-	data[4]=50;
-	for (i=0;i<5;i++)
-		printf("data[%i]= %i\n",i,data[i]);
-	return 0;
-}
+ 	
+ 		int i;
+ 		int grade[5];
+ 		int score[5];
+ 		
+ 		for(i=0;i<5;i++)
+ 		{
+ 			grade[i]=rand()%100;
+		 }
+ 		for (i=0;i<5;i++)
+		{
+		 	score[i]=grade[i];
+		}	
+ 		for(i=0;i<5;i++)
+		{
+		 	printf("score[%d]=%d\n",i,score[i]);
+		}	
+		return 0;
+	}	
+
